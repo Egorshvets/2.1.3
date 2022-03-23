81b05b07508294ad4f4ac712b84442f33c30cf1d
Egorshvets <86877552+Egorshvets@users.noreply.github.com>
2022-03-23 16:41:34 +0300

    Initial commit

---
+++ src/main/java/app/model/Timer.java
@@ -0,0 +1,22 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Timer {
+    public Long getNanoTime() {
+        return nanoTime;
+    }
+
+    public void setNanoTime(Long nanoTime) {
+        this.nanoTime = nanoTime;
+    }
+
+    private Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
